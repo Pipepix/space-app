79d0e088ff3bb7d475f297733893b70be68c1367
Facundo <facundoguzman54@gmail.com>
2023-10-08 09:41:31 -0300

    videos del index

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const formulario = document.getElementById('formulario-name');
+const videosource = document.getElementById('video-inicio');
+
+const buttonNav= document.getElementById('button-nav');
+
+function cambiarvideos(video, url){
+    video.onended = (event) => {
+        video.src = url; 
+        video.load(); 
+        video.play(); 
+    
+      };
+}
+formulario && formulario.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+    const nombre = document.getElementById('name').value;
+    console.log(nombre)
+    const formData = {
+        nombre: nombre,
+    };
+     nombre.value = "";
+     sessionStorage.setItem('formData', JSON.stringify(formData));
+     /* location.href = '/pages/form.html' */
+})
+
+
+
+cambiarvideos(videosource, "./img/inicio2.mp4");
+cambiarvideos(videosource, "./img/inicio2.mp4");
