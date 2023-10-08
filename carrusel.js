f8bf5c754993c7d076a2edaa1d7cd86ce63fe45f
Facundo <facundoguzman54@gmail.com>
2023-10-08 15:41:16 -0300

    Arreglo del carrusel

--- carrusel.js
+++ carrusel.js
@@ -1,19 +1,35 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+const prevbutton = document.getElementById("prev")
+const next = document.getElementById("next")
+
+
+
+function plusSlides(n) {
+  let slides = document.getElementsByClassName("mySlides")
+  showSlides(slideIndex += n);
+  if(slideIndex == slides.length) {
+    setTimeout( ()=>{
+      document.getElementById("button-nav").click()
+    }, 5000);
+    
+ 
+  }
 }
 
-function showSlides(n, no) {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); 
 }
